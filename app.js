f18d6b46f8acf28f7fda8816ebf50548c83e5f5c
Varma Cephas <varmac231@gmail.com>
2023-06-01 09:44:54 +0000

    js assignment answers

---
+++ app.js
@@ -0,0 +1,26 @@
+// problem 1
+let x = 8;
+let y = 8;
+console.log("The answer is" + " " + x%y);
+
+// problem 2
+let first_name = "Harry";
+console.log("My name is" + " " + first_name);
+
+
+// problem 3
+let num1 = 10;
+let num2 = 5;
+let sum = num1 + num2;
+console.log("The sum is" + " " + sum);
+
+// problem 4
+let radius = 4;
+let area = 3.14 * (4 ** 2);
+console.log("The area is" + " " + area);
+
+
+// problem 5
+let num = 9;
+num++
+console.log("The increment of num is" + " " + num);
